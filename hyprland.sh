20f5a544bc1742728e843a458d1adbf85c5dd89b
InvisibleBox <tapichar@gmail.com>
2024-07-24 20:38:29 +0700

    Leave edit the value of config for wallpaper

--- hyprland.sh
+++ hyprland.sh
@@ -14,7 +14,7 @@ sed -i 's/terminal = kitty/terminal = mate-terminal --hide-menubar/g;s/wofi --sh
 sed -i '/^# exec-once = \$terminal/c\\exec-once = picom --config ~/.config/picom/picom.conf' $hYPRFILE
 sed -i '/^# exec-once = waybar/c\\exec-once = waybar & hyprpaper' $hYPRFILE
 sed -i 's/gaps_in = 5/gaps_in = 4/g;s/gaps_out = 20/gaps_out = 6/g;s/border_size = 2/border_size = 1/g;s/rounding = 10/rounding = 7/g' $hYPRFILE
-sed -i 's/disable_hyprland_logo = false/disable_hyprland_logo = true/g' $hYPRFILE
+# sed -i 's/disable_hyprland_logo = false/disable_hyprland_logo = true/g' $hYPRFILE
 sed -i 's/\$mainMod, Q/\$mainMod, Return/g' $hYPRFILE
 sed -i 's/\$mainMod, M, exit/\$mainMod, Q, exit/g' $hYPRFILE
 sed -i 's/\$mainMod, C, killactive/\$mainMod SHIFT, X, killactive/g' $hYPRFILE
